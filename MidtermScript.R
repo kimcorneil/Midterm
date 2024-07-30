71661ac86f606d50cc72a7e304653ed7213d218f
kimcorneil <kcorneil3713@gmail.com>
2024-07-30 11:06:18 -0400

    Create script for midterm and load the required data

---
+++ MidtermScript.R
@@ -0,0 +1,11 @@
+# Script for the midterm
+# August 5, 2024
+# BTC1855H
+# Kimberly Corneil 
+
+# Load the data
+trip <- read.csv("trip.csv")
+station <- read.csv("station.csv")
+weather <- read.csv("weather.csv")
+
+
