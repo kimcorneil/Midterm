3fadc2a892a00177e7fe2b32c5a8bb233ba6ac07
kimcorneil <kcorneil3713@gmail.com>
2024-07-30 19:51:47 -0400

    Install required packages for EDA

--- MidtermScript.R
+++ MidtermScript.R
@@ -8,4 +8,12 @@ trip <- read.csv("trip.csv")
 station <- read.csv("station.csv")
 weather <- read.csv("weather.csv")
 
+####### Exploratory Data Analysis #######
+# Follow the blog post outlined in the assignment description
+# Install the required packages and add them to library
+library(tidyverse)
+install.packages("funModeling")
+install.packages("Hmisc")
+library(funModeling)
+library(Hmisc)
 
