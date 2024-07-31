63f933df1c6fff462018b2ea6c6b19f9fcdc4080
kimcorneil <kcorneil3713@gmail.com>
2024-07-30 23:54:33 -0400

    Removed observations that were suspected to be cancelled trips

--- MidtermScript.R
+++ MidtermScript.R
@@ -9,10 +9,26 @@ station <- read.csv("station.csv")
 weather <- read.csv("weather.csv")
 
 ####### Cleaning the data #######
-# Remove any trip starting or ending at the same station with a duration of less than 3 minutes from 'trip'
+
+# Remove any trip starting and ending at the same station with a duration of less than 3 minutes from 'trip'
 ## create trip2 so the original data set 'trip' remains unaltered
 trip2 <- trip
 ## convert start_station_id and end_station_id to character
 trip2$start_station_id <- as.character(trip2$start_station_id)
 trip2$end_station_id <- as.character(trip2$end_station_id)
+## convert duration from integer to numeric
+trip2$duration <- as.numeric(trip2$duration)
 ## since duration has a minimum of 60 and the units were not reported, I am making the assumption it was reported in seconds
+## replace any trips starting and ending at the same station with a duration less than 3 minutes (180 seconds) with NA
+trip2 <- trip2 %>% mutate(duration = 
+                            case_when(duration < 180 & end_station_id == start_station_id ~ NA, # replace any conditions in duration as specified with NA
+                                      .default = duration)) # if condition is not met the default is to maintain the value in duration
+## move all the rows that have NAs in duration to a separate data frame (cancelled_trips) before removing them 
+cancelled_trips <- trip2 %>% filter(is.na(duration))
+## remove any rows in trip2 that have NAs in duration
+trip2 <- trip2 %>% drop_na(duration)
+## summarize trip2 to see changes
+summary(trip2)
+
+
+
