6081b7c1148f7bdb042e97a292ad352478836f88
kimcorneil <kcorneil3713@gmail.com>
2024-08-02 10:47:46 -0400

    Made the script and the branch I will work in then got intimidated and gave up, will come back later.

--- MidtermScript.R
+++ MidtermScript.R
@@ -8,4 +8,8 @@ trip <- read.csv("trip.csv")
 station <- read.csv("station.csv")
 weather <- read.csv("weather.csv")
 
+####### Weather and bike rental correlation #######
+View(weather)
+summary(weather)
 
+View(station)
