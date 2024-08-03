6e96f16d0d89f5934b8fad86818f529af1fe9150
kimcorneil <kcorneil3713@gmail.com>
2024-08-02 23:04:34 -0400

    Data is ready to go for correlation analysis. It is prepared as only numeric variables and has been divided into 5 data frames based on the city the bike ride started in

--- MidtermScript.R
+++ MidtermScript.R
@@ -88,14 +88,41 @@ start_weather <- weather2 %>% left_join(summary_start, by = c("date" = "start_da
 
 # clean start_weather so it is ready to go for correlation analysis
 summary(start_weather)
+# events is not a numerical category and will cause issues in the correlation analysis
+## make events a factor to see what we are dealing with
+start_weather$events <- factor(start_weather$events)
+table(start_weather$events)
+### the levels of events are not well defined. If there was rain or fog or a rain-thundertorm (only 1) I hope it is dealt with in the other colums
+
 ## lots of issues with precipitation_inches 
-## set T to 0.001, this comes WITH MANY ERRORS I WILL TALK ABOUT IN MY REPORT
-
-
-
+## set T to 0.011, this comes WITH MANY ERRORS I WILL TALK ABOUT IN MY REPORT
+# Replace T with 0.011
+## first find where T is
+TT <- which(start_weather$precipitation_inches == "T")
+start_weather$precipitation_inches <- replace(start_weather$precipitation_inches, TT, 0.011)
+# make precipitation numeric
 start_weather$precipitation_inches <- as.numeric(start_weather$precipitation_inches)
 
-# cor plot time
+# remove events for correlation
+# remove columns added that were not needed
+start_weather2 <- start_weather[,!(names(start_weather) %in% c("events"))]
+
+# make city a factor
+start_weather2$city <- factor(start_weather2$city)
+
+# make a data frame for each city for the correlation analysis and only allow numeric columns
+Mountain_view <- start_weather2 %>% filter(city == "Mountain View") %>%
+  select_if(is.numeric)
+Palo_alto <- start_weather2 %>% filter(city == "Palo Alto") %>%
+  select_if(is.numeric)
+Redwood_city <- start_weather2 %>% filter(city == "Redwood City") %>%
+  select_if(is.numeric)
+San_fran <- start_weather2 %>% filter(city == "San Francisco") %>%
+  select_if(is.numeric)
+San_jose <- start_weather2 %>% filter(city == "San Jose") %>%
+  select_if(is.numeric)
+
+####### Run correlation analysis #######
 # install corrplot
 install.packages("corrplot")
 # add it to library
